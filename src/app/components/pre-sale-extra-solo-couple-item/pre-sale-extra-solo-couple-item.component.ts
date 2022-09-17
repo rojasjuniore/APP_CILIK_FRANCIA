89534e6654b85a6b416eabf4a07b26ce69cd2624
Jhonny <jhonnycgarcia@gmail.com>
2022-09-17 01:26:35 -0500

    UP

--- src/app/components/pre-sale-extra-solo-couple-item/pre-sale-extra-solo-couple-item.component.ts
+++ src/app/components/pre-sale-extra-solo-couple-item/pre-sale-extra-solo-couple-item.component.ts
@@ -1,28 +1,47 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { from, Observable, Subject } from 'rxjs';
+import { HotelService } from 'src/app/services/hotel.service';
 
 @Component({
   selector: 'app-pre-sale-extra-solo-couple-item',
   templateUrl: './pre-sale-extra-solo-couple-item.component.html',
   styleUrls: ['./pre-sale-extra-solo-couple-item.component.css']
 })
-export class PreSaleExtraSoloCoupleItemComponent implements OnInit {
+export class PreSaleExtraSoloCoupleItemComponent implements OnInit, OnChanges {
 
-  @Input() price = 65;
   @Input() min = 0;
   @Input() max = 99;
   @Input() quantity = 0;
 
   @Output() onUpdateQuantity = new Subject();
 
-  constructor() { }
+  public document$!: Observable<any>;
+
+  constructor(
+    private hotelSrv: HotelService,
+  ) { }
 
   ngOnInit(): void {
+    this.document$ = from(this.hotelSrv.getCategoryPassesByCode('couple'));
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    const { quantity } = changes;
+    if (quantity) {
+      this.quantity = quantity.currentValue;
+    }
   }
 
-  updateQuantity(quantity: any) {
+  updateQuantity(params: any) {
+    const { data, quantity } = params;
     this.quantity = quantity;
-    this.onUpdateQuantity.next({type: 'couple', quantity});
+    this.onUpdateQuantity.next({
+      type: 'couple', 
+      quantity, 
+      price: data.price,
+      fullPrice: data.fullPrice,
+      label: data.label,
+    });
   }
 
 }
