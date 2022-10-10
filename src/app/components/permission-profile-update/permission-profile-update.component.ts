0af840e9521a67c0636378bc5e28b0811e80666b
Jhonny <jhonnycgarcia@gmail.com>
2022-10-10 15:00:36 -0500

    UP

--- src/app/components/permission-profile-update/permission-profile-update.component.ts
+++ src/app/components/permission-profile-update/permission-profile-update.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Observable, of } from 'rxjs';
 import { BsModalService } from 'src/app/services/bs-modal.service';
+import { PermissionService } from 'src/app/services/permission.service';
 
 @Component({
   selector: 'app-permission-profile-update',
@@ -13,9 +15,12 @@ export class PermissionProfileUpdateComponent implements OnInit {
 
   public roles$!: Observable<any[]>;
   public profile: any;
+  public roleToAdd: any = null;
 
   constructor(
     private bsModalSrv: BsModalService,
+    private permissionSrv: PermissionService,
+    private spinner: NgxSpinnerService,
   ) { }
 
   ngOnInit(): void {
@@ -28,12 +33,45 @@ export class PermissionProfileUpdateComponent implements OnInit {
 
 
   async show(profile: any){
-    this.profile = profile;
+    const { roles = [] } = profile;
+    this.profile = Object.assign({}, profile, { roles });
+    this.roles$ = this.permissionSrv.getRolesDynamic();
     this.mi.show();
   }
 
   async hide(){
+    this.roles$ = of([]);
     this.mi.hide();
+  }
+
+  async addRole(){
+    if(!this.roleToAdd) return;
+
+    await this.spinner.show();
+
+    const { roles = [] } = this.profile;
+    roles.push(this.roleToAdd);
+    await this.permissionSrv.updateUserRoles(this.profile.uid, roles);
+    this.profile = Object.assign({}, this.profile, { roles });
+
+    this.spinner.hide();
+  }
+
+  checkUserHasRole(role: any){
+    const { roles = [] } = this.profile;
+    return Object.values(roles).includes(role);
+  }
+
+  async removeRole(role: any){
+    await this.spinner.show();
+
+    const { roles = [] } = this.profile;
+    const index = roles.indexOf(role);
+    roles.splice(index, 1);
+    await this.permissionSrv.updateUserRoles(this.profile.uid, roles);
+    this.profile = Object.assign({}, this.profile, { roles });
+
+    this.spinner.hide();
   }
 
 }
