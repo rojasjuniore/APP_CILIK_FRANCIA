abd3eb25c9aa2fb40b4a651d8a22745685cdd085
Jhonny <jhonnycgarcia@gmail.com>
2022-10-13 01:29:11 -0500

    UP

--- src/app/admin/components/list-order/list-order.component.ts
+++ src/app/admin/components/list-order/list-order.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
 import { PurchaseSummaryModalDetailsComponent } from 'src/app/components/purchase-summary-modal-details/purchase-summary-modal-details.component';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { HotelService } from 'src/app/services/hotel.service';
+import { PurchaseService } from 'src/app/services/purchase.service';
 
 @Component({
   selector: 'app-list-order',
@@ -16,14 +18,51 @@ export class ListOrderComponent implements OnInit {
   public listOrdersPending: any = [];
   public listOrdersCompleted: any = [];
   public listOrdersRejected: any = [];
+
+  public orderListP$!: Observable<any[]>;
+  public orderListC$!: Observable<any[]>;
+  public orderListR$!: Observable<any[]>;
+
   loading = false;
   show = false;
 
-  constructor(private hotelService: HotelService,
-              private authSrv: AuthenticationService) { localStorage.setItem('auth', 'adm') }
+  constructor(
+    private hotelService: HotelService,
+    private purchaseSrv: PurchaseService,
+    private authSrv: AuthenticationService
+  ) { 
+    localStorage.setItem('auth', 'adm') 
+  }
 
   ngOnInit(): void {
-    this.getOrderList();
+
+    /** Ordenes Pagos por transferencia pendientes */
+    this.orderListP$ = this.purchaseSrv.getDynamic([
+      {field: 'paymentMethodType', condition: '==', value: 'bankTransfer'},
+      {field: 'status', condition: '==', value: 'pending'},
+    ], {
+      orderBy: [{field: 'createdAt', order: 'asc'}]
+    });
+
+    /** Ordenes Pagos por transferencia completadas */
+    this.orderListC$ = this.purchaseSrv.getDynamic([
+      {field: 'paymentMethodType', condition: '==', value: 'bankTransfer'},
+      {field: 'status', condition: '==', value: 'completed'},
+    ], {
+      orderBy: [{field: 'createdAt', order: 'desc'}]
+    });
+
+    /** Ordenes Pagos por transferencia rechazadas */
+    this.orderListR$ = this.purchaseSrv.getDynamic([
+      {field: 'paymentMethodType', condition: '==', value: 'bankTransfer'},
+      {field: 'status', condition: '==', value: 'rejected'},
+    ], {
+      orderBy: [{field: 'createdAt', order: 'desc'}]
+    });
+
+
+
+    // this.getOrderList();
   }
 
 
