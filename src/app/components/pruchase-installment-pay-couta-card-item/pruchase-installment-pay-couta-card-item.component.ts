910867babc45e81305509a41c7c01e6e1e738633
Jhonny <jhonnycgarcia@gmail.com>
2022-11-07 19:54:57 -0400

    UP - Se revierte cambio.

--- src/app/components/pruchase-installment-pay-couta-card-item/pruchase-installment-pay-couta-card-item.component.ts
+++ src/app/components/pruchase-installment-pay-couta-card-item/pruchase-installment-pay-couta-card-item.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
-import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { Sweetalert2Service } from 'src/app/services/sweetalert2.service';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-pruchase-installment-pay-couta-card-item',
@@ -18,24 +16,23 @@ export class PruchaseInstallmentPayCoutaCardItemComponent implements OnInit {
 
   constructor(
     private sweetAlert2Srv: Sweetalert2Service,
-    private router: Router,
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.showPaymentMethods();
+  }
 
   async alreadyPaid() {
     return this.sweetAlert2Srv.showInfo('Esta cuota ya fue pagada');
   }
 
   async showPaymentMethods() {
-    const { url } = this.item;
-    // console.log('url', url);
-
-    /** Eliminar de la cadena baseURL */
-    const newUrl = url.replace(environment.urlWeb,'');
-    console.log(newUrl);
-
-    return this.router.navigate([ '/' + newUrl]);
+    return this.onSelectShowPaymentMethods.next({
+      item: this.item,
+      index: this.index,
+      type: this.item.paymentMethod,
+      payed: this.item.payed,
+    });
   }
 
 }
