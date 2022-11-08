a7ae71093f6b58955e8e0e610f5002a3240d53d7
Jhonny <jhonnycgarcia@gmail.com>
2022-11-07 20:00:19 -0400

    UP

--- src/app/components/pre-sale-installment-couta-card-item/pre-sale-installment-couta-card-item.component.ts
+++ src/app/components/pre-sale-installment-couta-card-item/pre-sale-installment-couta-card-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-pre-sale-installment-couta-card-item',
@@ -9,10 +11,23 @@ export class PreSaleInstallmentCoutaCardItemComponent implements OnInit {
 
   @Input() index: number = 0;
   @Input() item: any;
+  @Input() showButton = false;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  async goToPayCuota(){
+    const { url } = this.item;
+
+    /** Eliminar de la cadena baseURL */
+    const newUrl = url.replace(environment.urlWeb,'');
+    console.log(newUrl);
+
+    return this.router.navigate([ '/' + newUrl]);
   }
 
 }
