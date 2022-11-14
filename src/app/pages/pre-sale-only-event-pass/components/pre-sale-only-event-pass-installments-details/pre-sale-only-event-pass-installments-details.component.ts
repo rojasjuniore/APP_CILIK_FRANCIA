1769d1a3f22e4c20ccf6f0d8e96a3ad930f83f8c
Jhonny <jhonnycgarcia@gmail.com>
2022-11-14 15:34:26 -0400

    UP - Se habilita componente para visualizar detalles de cuotas en orden de compra al momento de generar la orden.

--- src/app/pages/pre-sale-only-event-pass/components/pre-sale-only-event-pass-installments-details/pre-sale-only-event-pass-installments-details.component.ts
+++ src/app/pages/pre-sale-only-event-pass/components/pre-sale-only-event-pass-installments-details/pre-sale-only-event-pass-installments-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PreSaleService } from 'src/app/services/pre-sale.service';
 
 @Component({
   selector: 'app-pre-sale-only-event-pass-installments-details',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreSaleOnlyEventPassInstallmentsDetailsComponent implements OnInit {
 
-  constructor() { }
+  public installments!: any[];
 
-  ngOnInit(): void {
+  /** Documento de orden de compra desde el localStorage */
+  public preSaleDocument: any;
+
+  constructor(
+    private preSaleSrv: PreSaleService,
+    private router: Router,
+  ) {
+    const {installments} = this.preSaleSrv.checkAndLoadDocumentLocalStorage();
+    this.installments = installments;
+  }
+
+  ngOnInit(): void {}
+
+  onBack(){
+    this.preSaleSrv.updateDocumentLocalStorage({step: '/pre-sale-event-pass/payment-method'});
+    this.router.navigate(['/pre-sale-event-pass/payment-method']);
+  }
+
+  onNext(){
+    this.preSaleSrv.updateDocumentLocalStorage({step: '/pre-sale-event-pass/installments-pay'});
+    this.router.navigate(['/pre-sale-event-pass/installments-pay']);
   }
 
 }
