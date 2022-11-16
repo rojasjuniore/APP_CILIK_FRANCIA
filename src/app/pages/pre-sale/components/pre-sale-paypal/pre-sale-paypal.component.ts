339f1fe8fba523b9c505bae15bbf495f045c208e
Jhonny <jhonnycgarcia@gmail.com>
2022-11-15 21:38:37 -0400

    UP

--- src/app/pages/pre-sale/components/pre-sale-paypal/pre-sale-paypal.component.ts
+++ src/app/pages/pre-sale/components/pre-sale-paypal/pre-sale-paypal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { purchaseTotales } from 'src/app/helpers/purchase-totales.helper';
 import { PreSaleService } from 'src/app/services/pre-sale.service';
 import { PurchaseService } from 'src/app/services/purchase.service';
 
@@ -26,48 +27,7 @@ export class PreSalePaypalComponent implements OnInit {
 
 
   get total(){
-
-    const nroParticipantsByRoom = this.preSaleDocument.rooms
-    .map((room: any) => room.capacity)
-    .reduce((a: number, b: number) => a + b, 0);
-
-    const roomsAmount = this.preSaleDocument.rooms
-      .map((room: any) => room.price)
-      .reduce((a: number, b: number) => a + b, 0);
-
-    const additionalDaysAmount = this.preSaleDocument.rooms
-      .map((room) => room.additionals)
-      .filter((row) => row.length > 0)
-      .map((data) => data.map((row) => row.quantity * row.price).reduce((prev, curr) => prev + curr, 0))
-      .reduce((prev, curr) => prev + curr, 0);
-
-    const additionalCategoryPasses = this.preSaleDocument.additionalCategoryPasses
-      .map((row) => {
-        if(row.type == 'group'){
-          return row.data.map((group) => group.quantity * group.price)
-            .reduce((prev, curr) => prev + curr, 0)
-  
-        }else{
-          return row.quantity * row.price;
-        }
-      })
-      .reduce((prev, curr) => prev + curr, 0)
-
-    const subTotal = [roomsAmount, additionalDaysAmount, additionalCategoryPasses]
-    .reduce((prev, curr) => prev + curr, 0);
-
-    /**
-     * Calcular descuento por grupo
-     */
-     let groupDiscount = 0;
-     if(nroParticipantsByRoom >= 20){
-       groupDiscount = subTotal * 0.10;
-     }else if(nroParticipantsByRoom >= 10){
-       groupDiscount = subTotal * 0.05;
-     }
-
-    const total = subTotal - groupDiscount;
-    return total;
+    return purchaseTotales(this.preSaleDocument).total;
   }
 
   async onPaypalResponse(params: any){
