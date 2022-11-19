c2b0cd188c01d1884ed872ab40b9a715db5d947f
Jhonny <jhonnycgarcia@gmail.com>
2022-11-18 23:40:19 -0400

    UP

--- src/app/pages/pre-sale-only-event-pass/components/pre-sale-only-event-pass-paypal/pre-sale-only-event-pass-paypal.component.ts
+++ src/app/pages/pre-sale-only-event-pass/components/pre-sale-only-event-pass-paypal/pre-sale-only-event-pass-paypal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import moment from 'moment';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { purchaseTotales } from 'src/app/helpers/purchase-totales.helper';
 import { PreSaleService } from 'src/app/services/pre-sale.service';
@@ -57,7 +58,7 @@ export class PreSaleOnlyEventPassPaypalComponent implements OnInit {
       await this.spinner.show();
 
       /** Procesar orden y retornar enlace de redirección */
-      const url = await this.preSaleSrv.completePreSaleOrder(metadata);
+      const url = await this.preSaleSrv.completePreSaleOrder(metadata, {payedAt: moment().valueOf()});
       this.router.navigate([url]);
       return;
       
