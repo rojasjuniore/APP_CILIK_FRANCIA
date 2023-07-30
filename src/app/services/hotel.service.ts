ab0ce7e8cceac41549598740e7db6fbc03655459
0x1junior <116573955+0x1junior@users.noreply.github.com>
2023-07-30 13:48:19 -0500

    melo

--- src/app/services/hotel.service.ts
+++ src/app/services/hotel.service.ts
@@ -8,18 +8,19 @@ import { getStorage, ref, deleteObject } from "firebase/storage";
 import { pick } from 'underscore';
 import moment from 'moment';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelService {
 
-  public purchases = 'purchases';
-  public roomTypesCollection = 'room__types';
-  public additionalsCollection = 'room__additionals';
-  public categoryPassesCollection = 'categoryPasses';
-  public roomStock = 'room__stock';
-  public roomsCollection = 'rooms';
+  public purchases = environment.production ? 'purchases' : 'purchases-dev';
+  public roomTypesCollection = environment.production ? 'room__types' : 'room__types-dev';
+  public additionalsCollection = environment.production ? 'room__additionals' : 'room__additionals-dev';
+  public categoryPassesCollection = environment.production ? 'categoryPasses' : 'categoryPasses-dev';
+  public roomStock = environment.production ? 'room__stock' : 'room__stock-dev';
+  public roomsCollection = environment.production ? 'rooms' : 'rooms-dev';
 
   loading = false;
 
@@ -28,7 +29,7 @@ export class HotelService {
     private storage: AngularFireStorage
   ) { }
 
-  buildRoomDoc(params: any = {}){
+  buildRoomDoc(params: any = {}) {
     return {
       roomId: params.roomId || null,
       nroParticipants: params.nroParticipants || 1,
@@ -38,7 +39,7 @@ export class HotelService {
     }
   }
 
-  parseRoomDefaultByCapacityDocument(doc: any){
+  parseRoomDefaultByCapacityDocument(doc: any) {
     return pick(doc, [
       'capacity',
       'price',
@@ -56,7 +57,7 @@ export class HotelService {
     ]);
   }
 
-  parseRoomPrice(room: any, type = 'fullPass'){
+  parseRoomPrice(room: any, type = 'fullPass') {
     const currentDate = moment();
     let price = room?.fullPrice;
     let indexPrice = room.priceList.length - 1;
@@ -74,7 +75,7 @@ export class HotelService {
       //   to: to.format('DD/MM/YYYY'),
       // });
 
-      if(isBetween && rowType === type){
+      if (isBetween && rowType === type) {
         indexPrice = index;
         break;
       }
@@ -88,7 +89,7 @@ export class HotelService {
   /** ===============================================
    *                  ROOM STOCK
   ================================================== */
-  async storeRoomStock(docId: string, data: any){
+  async storeRoomStock(docId: string, data: any) {
     return this.afs.collection(this.roomStock).doc(docId).set(data);
   }
 
@@ -96,13 +97,13 @@ export class HotelService {
     return this.afs.collection(this.roomStock).doc(docId).update(data);
   }
 
-  async updateRoomStockSupplyCounter(docId: string, data = 1){
+  async updateRoomStockSupplyCounter(docId: string, data = 1) {
     const ref = this.afs.collection(this.roomStock).doc(docId);
     await ref.update({ supply: increment(data) });
   }
 
-  getDynamicRoomCollection(where: any[] = [], opts: any = {}): Observable<any>{
-    const {idField = "_id", orderBy = []} = opts;
+  getDynamicRoomCollection(where: any[] = [], opts: any = {}): Observable<any> {
+    const { idField = "_id", orderBy = [] } = opts;
 
     return this.afs.collection(
       this.roomStock,
@@ -128,7 +129,7 @@ export class HotelService {
   }
 
 
-  async getRoomDefaultByCapacity(capacity: number, type = 'fullPass'){
+  async getRoomDefaultByCapacity(capacity: number, type = 'fullPass') {
     const snapshot = await lastValueFrom(
       this.afs.collection(
         this.roomTypesCollection,
@@ -140,8 +141,8 @@ export class HotelService {
     return this.parseRoomPrice(result.shift(), type);
   }
 
-  getDynamicRoomTypeCollection(where: any[] = [], opts: any = {}): Observable<any>{
-    const {idField = "_id", orderBy = []} = opts;
+  getDynamicRoomTypeCollection(where: any[] = [], opts: any = {}): Observable<any> {
+    const { idField = "_id", orderBy = [] } = opts;
 
     return this.afs.collection(
       this.roomTypesCollection,
@@ -155,8 +156,8 @@ export class HotelService {
     ).valueChanges({ idField });
   }
 
-  async getDynamicRoomTypesCollectionPromise(where: any[] = [], opts: any = {}): Promise<any>{
-    const {idField = "_id", orderBy = []} = opts;
+  async getDynamicRoomTypesCollectionPromise(where: any[] = [], opts: any = {}): Promise<any> {
+    const { idField = "_id", orderBy = [] } = opts;
 
     const snapshot = await lastValueFrom(
       this.afs.collection(
@@ -164,7 +165,7 @@ export class HotelService {
         (ref) => {
           let query: Query = ref;
           for (const row of where) { query = query.where(row.field, row.condition, row.value); }
-  
+
           for (const order of orderBy) { query = query.orderBy(order.field, order.order); }
           return query;
         }
@@ -191,7 +192,7 @@ export class HotelService {
    * @param orderId 
    * @returns 
    */
-  async restoreRoomsOnReject(orderId: string){
+  async restoreRoomsOnReject(orderId: string) {
     const snapshot = await lastValueFrom(
       this.afs.collection(this.roomsCollection, (ref) => ref.where('paymentOrderID', '==', orderId)).get()
     );
@@ -205,15 +206,15 @@ export class HotelService {
     return true;
   }
 
-  async getAvailableRoomByCodeType(code: string){
+  async getAvailableRoomByCodeType(code: string) {
     const snapshot = await lastValueFrom(
-      this.afs.collection(this.roomsCollection, 
+      this.afs.collection(this.roomsCollection,
         (ref) => ref.where('code', '==', code)
           .where('paymentOrderID', '==', null)
           .orderBy('roomCode', 'asc')
           .limit(1)
-        ).get()
-      );
+      ).get()
+    );
 
     const result = await handlerArrayResult(snapshot);
     return (result.length > 0) ? result.shift() : null;
@@ -226,8 +227,8 @@ export class HotelService {
     return this.afs.collection(this.additionalsCollection).doc(docId).set(data);
   }
 
-  getDynamicAdditionalsCollection(where: any[] = [], opts: any = {}): Observable<any>{
-    const {idField = "_id", orderBy = []} = opts;
+  getDynamicAdditionalsCollection(where: any[] = [], opts: any = {}): Observable<any> {
+    const { idField = "_id", orderBy = [] } = opts;
 
     return this.afs.collection(
       this.additionalsCollection,
@@ -241,7 +242,7 @@ export class HotelService {
     ).valueChanges({ idField });
   }
 
-  async getAdditonalDaysByRoomCode(roomCode: string){
+  async getAdditonalDaysByRoomCode(roomCode: string) {
     const snapshot = await lastValueFrom(
       this.afs.collection(this.additionalsCollection).doc(roomCode).get()
     );
@@ -256,7 +257,7 @@ export class HotelService {
     return this.afs.collection(this.categoryPassesCollection).doc(docId).set(data);
   }
 
-  async getCategoriesPasses(){
+  async getCategoriesPasses() {
     const snapshot = await lastValueFrom(
       this.afs.collection(this.categoryPassesCollection, (ref) => ref.orderBy('order', 'asc')).get()
     );
@@ -264,7 +265,7 @@ export class HotelService {
     return result.map((row) => this.parseRoomPrice(row));
   }
 
-  async getCategoryPassesByCode(code: string){
+  async getCategoryPassesByCode(code: string) {
     const snapshot = await lastValueFrom(
       this.afs.collection(this.categoryPassesCollection).doc(code).get()
     );
@@ -274,7 +275,7 @@ export class HotelService {
 
   // Subida de img comprobantes
 
-  async uploadComprobantes(img, idOrden, order){
+  async uploadComprobantes(img, idOrden, order) {
     // if(imgTempCapture !== ''){
     //   console.log(imgTempCapture)
     //   this.deleteImgComprobante(idOrden);
@@ -283,7 +284,7 @@ export class HotelService {
     let fileRef = this.storage.ref(filePath);
     let observe = this.storage.upload(filePath, img);
     return observe.snapshotChanges().pipe(
-      finalize( () => {
+      finalize(() => {
         fileRef.getDownloadURL().subscribe({
           next: (url) => {
             console.log(url)
@@ -300,8 +301,8 @@ export class HotelService {
 
   }
 
-  async updateCaptureImgComprobante(idOrden, order){
-    const ref =  this.afs.collection(this.purchases).doc(idOrden)
+  async updateCaptureImgComprobante(idOrden, order) {
+    const ref = this.afs.collection(this.purchases).doc(idOrden)
     await ref.update({ metadata: order.metadata });
   }
 
@@ -321,7 +322,7 @@ export class HotelService {
   // }
 
 
-  getOrderPending(){
+  getOrderPending() {
     // , ref =>   ref.where('status', '==', 'pending')
     return this.afs.collection(this.purchases).valueChanges()
   }
@@ -329,12 +330,12 @@ export class HotelService {
 
 
   async updateOrder(docId: string, data: any) {
-    if(data.status === 'rejected'){
-      this.afs.collection(this.roomsCollection, ref =>   ref.where('paymentOrderID', '==', docId)).valueChanges().subscribe({
+    if (data.status === 'rejected') {
+      this.afs.collection(this.roomsCollection, ref => ref.where('paymentOrderID', '==', docId)).valueChanges().subscribe({
         next: (resp: any) => {
           console.log(resp)
-          if(resp && resp.length > 0){
-            this.afs.collection(this.roomsCollection).doc(resp[0].roomCode).update({paymentOrderID: '', additionals: []});
+          if (resp && resp.length > 0) {
+            this.afs.collection(this.roomsCollection).doc(resp[0].roomCode).update({ paymentOrderID: '', additionals: [] });
           }
         }
       })
