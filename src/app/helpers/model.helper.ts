1ca01f4de92e8f844f461896cfb1ac0b17d29be6
Jhonny <jhonnycgarcia@gmail.com>
2023-09-14 12:49:54 -0500

    UP - Se realizan cambios en helper de modelo. - Se definen nuevos métodos para manipular resultados de consultas de Realtime Database.

--- src/app/helpers/model.helper.ts
+++ src/app/helpers/model.helper.ts
@@ -36,6 +36,26 @@ export async function handlerObjectResult(snapshot: any, opts: any = {}){
     return Object.assign({}, snapshot.data(), {[idField]: snapshot.id});
 }
 
+export async function handlerArrayResultRDB(snapshot: any, opts: any = {}){
+    const {idField = "_id"} = opts;
+    let result: any[] = [];
+    
+    if(snapshot.numChildren() == 0){ return result; }
+
+    snapshot.forEach((doc: any) => {
+        result.push(
+            Object.assign({}, doc.val(), {[idField]: doc.key})
+        );
+    });
+    
+    return result;
+}
+
+export async function handlerObjectResultRDB(snapshot, opts: any = {}){
+    const {idField = "_id"} = opts;
+    if(!snapshot.exists()){ return null; }
+    return Object.assign({}, snapshot.val(), {[idField]: snapshot.key});
+}
 
 
 
