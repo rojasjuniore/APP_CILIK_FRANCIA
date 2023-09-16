a0a61fbf79595d9869e7af54e558e9bcea01e83e
Jhonny <jhonnycgarcia@gmail.com>
2023-09-16 16:50:48 -0500

    UP

--- src/app/out-pages/sign-up/sign-up.component.ts
+++ src/app/out-pages/sign-up/sign-up.component.ts
@@ -10,6 +10,10 @@ import { TemporalTokenService } from 'src/app/services/temporal-token.service';
 import { EmailNotificationService } from 'src/app/services/email-notification.service';
 import { CommonService } from 'src/app/services/common.service';
 import { TranslatePipe } from '@ngx-translate/core';
+import { CustomizationfileService } from 'src/app/services/customizationfile/customizationfile.service';
+import { QuickNotificationService } from 'src/app/services/quick-notification/quick-notification.service';
+import moment from 'moment';
+import { DocumentService } from 'src/app/services/documents/document.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -100,8 +104,10 @@ export class SignUpComponent implements OnInit {
     private commonService: CommonService,
     private sweetAlert2Srv: Sweetalert2Service,
     private temporalTokenSrv: TemporalTokenService,
-    private emailNotificationSrv: EmailNotificationService,
+    private quickNotificationSrv: QuickNotificationService,
     private translatePipe: TranslatePipe,
+    private _cf: CustomizationfileService,
+    private documentSrv: DocumentService
   ) {
 
     /** Phone number prefix list */
@@ -193,13 +199,71 @@ export class SignUpComponent implements OnInit {
         Validators.maxLength(12)
       ]],
       confirmPassword: ['', Validators.required],
-      termsAndCondition: [false, [Validators.requiredTrue]],
+      tac: [false, [Validators.requiredTrue]],
       imageUseAuthorization: [false, [Validators.requiredTrue]],
       informedConsentForMinors: [false, [Validators.requiredTrue]],
     }, { validator: MustMatch('password', 'confirmPassword') });
   }
 
   get f() { return this.form.controls; }
+
+  /**
+   * Guardar perfil del nuevo usuario
+   *
+   * @param profile
+   * @param userNew
+   */
+  async saveProfile(profile: any, userNew: any) {
+    /**
+     * - Extraer UID del usuario
+     * - Validar si existe
+     */
+    const uid = userNew.user.uid;
+    if (!uid) { throw new Error("NO UID"); }
+
+    /**
+      * - Convertir a string numero de identificación
+      * - Convertir a minisculas nombre completo
+      * - Convertir a minusculas dirección de email
+      * - Definir imagen base de avatar
+      */
+    const identification = this._cf.transformarString(profile.identification);
+    const name = `${profile.name} ${profile.surnames}`.toLowerCase();
+    const email = profile.email.toLowerCase();
+    const avatar = '/assets/img/002-man.svg';
+    const identificationType = profile.identificationType;
+
+
+    /**
+      * - Crear documento usuario
+      * - Crear documento perfil de usuario
+      */
+    const [userDoc, profileDoc] = await Promise.all([
+      this.authenticationSrv.buildAndStoreUserDoc(Object.assign({}, profile, { uid, avatar, name, identification, email, identificationType })),
+      this.authenticationSrv.buildAndStoreProfileDoc(Object.assign({}, profile, { uid, email, identification, identificationType }))
+    ]);
+
+
+    /**
+     * - Crear documento de perfil para Realtime
+     * - Almacenar perfil en Realtime
+     * - Almacenar identificador de token push del usuario creado
+     */
+    const profileDocReal = Object.assign(userDoc, { profile: profileDoc });
+    // console.log("profileDocReal", profileDocReal);
+    await this.authenticationSrv.db.object(`users/${uid}`).update(profileDocReal),
+
+    /**
+     * - Almacenar identificador del usuario en el localStorage
+     * - Almacenar documento del perfil en el localStorage
+     * - Almacenar imagen de avatar en localStorage
+     */
+    localStorage.setItem("uid", uid);
+    localStorage.setItem("profile", JSON.stringify(profileDoc));
+    localStorage.setItem("avatar", avatar);
+
+    return true;
+  }
 
   async onSubmit() {
     try {
@@ -213,14 +277,6 @@ export class SignUpComponent implements OnInit {
       }
 
       const formData = this.form.value;
-
-      const checkDNI = await this.authenticationSrv.checkDNI(formData.dni, formData.identificationType);
-      if (checkDNI) {
-        this.f.dni.setErrors({ dniStored: true });
-        return;
-      }
-
-
       const data = {
         name: this.commonService.noSpecialCharacters(formData.name),
         surnames: this.commonService.noSpecialCharacters(formData.surnames),
@@ -229,24 +285,11 @@ export class SignUpComponent implements OnInit {
         prefijo: formData.prefijo,
         phone: formData.phone,
         email: `${formData.email}`.trim().toLowerCase(),
-        roles: ['user'],
-        language: 'en'
       };
-      console.log('try to submit', data);
-
-
-      /** valid document */
-      const validDocument = await this.authenticationSrv.getByDocument(data.identification, data.identificationType);
-      console.log('validDocument', validDocument);
-      if (validDocument != null) {
-        const message = this.translatePipe.transform('formValidations.dniStored');
-        this.sweetAlert2Srv.showWarning(message);
-        return
-      }
-
+      // console.log('try to submit', data);
 
       /** Ejecutar 2FA en proceso de registro */
-      const run2FA = await this.temporalTokenSrv.runByEmail(formData.email);
+      const run2FA = await this.temporalTokenSrv.runByEmail(data.email);
       if (!run2FA) {
         return;
       }
@@ -255,32 +298,32 @@ export class SignUpComponent implements OnInit {
 
       /** Crear usuario en sistema de autenticación */
       const afsUser = await this.authenticationSrv.createUserWithEmailAndPassword(data.email, password);
-      console.log('afsUser', afsUser);
+      // console.log('afsUser', afsUser);
 
-      const uid = afsUser.user?.uid;
-
-      /**
-       * TODO: estructura de la base de datos que tiene settle/cilik v2 -
-       */
-      /** Registrar usuario */
-      // await this.authenticationSrv.store(`${uid}`, data);
+      /** Guardar usuario - Utilizando la estructura de CILIK */
+      await this.saveProfile(formData, afsUser);
 
       /** Enviar mail de bienvenida */
-      await this.emailNotificationSrv.sendWelcomeNotification([data.name, data.surnames].join(' '), data.email);
-
-      /** Guardar identificador en el localStorage */
-      this.authenticationSrv.setLocalUID(uid);
-
-
-      /**
-       * TODO: este metodo se ajusta al la estrutura de la base de datos que tiene settle/cilik v1
-       */
-      await this.authenticationSrv.saveProfile(data, uid);
+      const names = `${data.name} ${data.surnames}`.toUpperCase();
+      await this.quickNotificationSrv.sendEmailNotification({
+        type: "2FANotification",
+        email: data.email,
+        subject: `WLDC Cartagena 2024 - Bienvenido a WLDC ${names} - ` + moment().format("DD/MM/YYYY HH:mm:ss"),
+        greeting: `¡Hola ${names}!`,
+        messageBody: [
+          {type: "html", html: `<h1 style='text-align: center;'><strong>¡Bienvenido a la aplicación WLDC Cartagena 2024!</strong></h1>`},
+          {type: 'line', text: `Estamos muy felices de contar con tu presencia en la edición WLDC 2024.`},
+          {type: 'line', text: `En la aplicación podrás realizar la compra de los paquetes para asistir a esta gran experiencia WLDC 2024 en Cartagena Colombia.`},
+          {type: "line", text: "Si no reconoce esta actividad, no se requiere ninguna acción adicional."}
+      ],
+        salutation: '¡Saludos!'
+      });
 
       const message = this.translatePipe.transform('general.successfulRegistration');
       this.sweetAlert2Srv.showSuccess(message, 2);
 
-      return this.router.navigate(['pages', 'dashboard']);
+      this.router.navigate(['pages', 'dashboard']);
+      return;
 
     } catch (err) {
       console.log('Error on SignUpComponent.onSubmit', err);
