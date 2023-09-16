5a99622a7d2eece6d0f5125136b3c4410db0b442
Jhonny <jhonnycgarcia@gmail.com>
2023-09-16 12:18:10 -0500

    UP

--- src/app/pages/dashboard/components/store/store.component.ts
+++ src/app/pages/dashboard/components/store/store.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { CartService } from 'src/app/services/cart.service';
+import { CustomizationfileService } from 'src/app/services/customizationfile/customizationfile.service';
 
 @Component({
   selector: 'app-store',
@@ -12,6 +15,7 @@ export class StoreComponent implements OnInit {
       isRecommended: true,
       img: 'assets/images/icons/plan-icon-1.png',
       title: 'Hotel y Evento',
+      slug: 'hotel-event',
       include: 'incluye texto',
       noInclude: null,
       headerColor: 'red',
@@ -20,6 +24,7 @@ export class StoreComponent implements OnInit {
       isRecommended: false,
       img: 'assets/images/icons/plan-icon-2.png',
       title: 'Full Pass',
+      slug: 'full-pass',
       include: 'incluye texto',
       noInclude: null,
       headerColor: 'red',
@@ -28,6 +33,7 @@ export class StoreComponent implements OnInit {
       isRecommended: false,
       img: 'assets/images/icons/plan-icon-2.png',
       title: 'Pase por día',
+      slug: 'day-pass',
       include: 'incluye texto',
       noInclude: null,
       headerColor: 'red',
@@ -36,6 +42,7 @@ export class StoreComponent implements OnInit {
       isRecommended: false,
       img: 'assets/images/icons/plan-icon-2.png',
       title: 'Pase de fin de semana',
+      slug: 'weekend-pass',
       include: 'incluye texto',
       noInclude: null,
       headerColor: 'red',
@@ -44,18 +51,23 @@ export class StoreComponent implements OnInit {
       isRecommended: false,
       img: 'assets/images/icons/plan-icon-2.png',
       title: 'Pases de Categoria',
+      slug: 'category-pass',
       include: 'incluye texto',
       noInclude: null,
       headerColor: 'red',
     },
   ];
 
-  constructor() { }
+  constructor(
+    private spinner: NgxSpinnerService,
+    private cartSrv: CartService,
+    private _cf: CustomizationfileService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  onSelectItem(item: any){
+  async onSelectItem(item: any){
     console.log(item);
   }
 
