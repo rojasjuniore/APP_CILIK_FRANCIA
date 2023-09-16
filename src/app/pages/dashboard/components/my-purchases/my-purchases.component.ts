b8f392ea173f1edec1825726d8e636bd5cec71a6
Jhonny <jhonnycgarcia@gmail.com>
2023-09-16 12:01:29 -0500

    UP

---
+++ src/app/pages/dashboard/components/my-purchases/my-purchases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-purchases',
+  templateUrl: './my-purchases.component.html',
+  styleUrls: ['./my-purchases.component.css']
+})
+export class MyPurchasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -6,13 +6,15 @@ import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { StoreComponent } from './components/store/store.component';
 import { StoreItemCardComponent } from './components/store-item-card/store-item-card.component';
+import { MyPurchasesComponent } from './components/my-purchases/my-purchases.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
     StoreComponent,
-    StoreItemCardComponent
+    StoreItemCardComponent,
+    MyPurchasesComponent
   ],
   imports: [
     CommonModule,
