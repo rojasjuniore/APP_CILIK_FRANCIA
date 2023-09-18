e421a6b441b2212eb08b2283593ce49cb08d6203
Jhonny <jhonnycgarcia@gmail.com>
2023-09-18 08:51:11 -0500

    UP

--- src/app/services/dedicates/event-info.service.ts
+++ src/app/services/dedicates/event-info.service.ts
@@ -11,26 +11,38 @@ export class EventInfoService {
   public eventDates: any[] = [
     {
       date: '2024-02-05',
+      weekend: false,
     },
     {
       date: '2024-02-06',
+      weekend: false,
     },
     {
       date: '2024-02-07',
+      weekend: false,
     },
     {
       date: '2024-02-08',
+      weekend: false,
     },
     {
       date: '2024-02-09',
+      weekend: true,
     },
     {
       date: '2024-02-10',
+      weekend: true,
     },
     {
       date: '2024-02-11',
+      weekend: true,
     },
   ];
 
   constructor() { }
+
+  getWeekendDays(){
+    return this.eventDates.filter((date) => date.weekend);
+  }
+
 }
