028cbe1b88b4d4b06a1775b642abd03c1c0c32f3
Jhonny <jhonnycgarcia@gmail.com>
2023-09-19 12:10:03 -0500

    UP

--- src/app/shared/hotel-and-event-room-list-item-card/hotel-and-event-room-list-item-card.component.ts
+++ src/app/shared/hotel-and-event-room-list-item-card/hotel-and-event-room-list-item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-hotel-and-event-room-list-item-card',
@@ -10,6 +11,8 @@ export class HotelAndEventRoomListItemCardComponent implements OnInit, OnChanges
   @Input() item: any = {}
   @Input() dates: any[] = [];
 
+  @Output() onSelectRoom = new Subject();
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,7 +20,14 @@ export class HotelAndEventRoomListItemCardComponent implements OnInit, OnChanges
 
   ngOnChanges(changes: SimpleChanges): void {
     const { dates } = changes;
-    if(dates && dates.currentValue){ this.dates = dates.currentValue; }
+    if(dates && dates.currentValue){
+      this.dates = dates.currentValue;
+      console.log('this.dates', this.dates);
+    }
+  }
+
+  selectRoom(){
+    this.onSelectRoom.next({...this.item, dates: this.dates});
   }
 
 }
