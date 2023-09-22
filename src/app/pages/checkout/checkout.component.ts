f5b56cb6ae2cc4c2616cc2dc04125503d76aa96e
Jhonny <jhonnycgarcia@gmail.com>
2023-09-21 19:29:39 -0500

    UP

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import moment from 'moment';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { Subscription, distinctUntilChanged, switchMap } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { CartService } from 'src/app/services/cart.service';
-import { PaypalButtonComponent } from 'src/app/shared/paypal-button/paypal-button.component';
+import { PurchaseService } from 'src/app/services/purchase.service';
+import { Sweetalert2Service } from 'src/app/services/sweetalert2.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -52,6 +56,10 @@ export class CheckoutComponent implements OnInit {
   constructor(
     private authSrv: AuthenticationService,
     private cartSrv: CartService,
+    private spinner: NgxSpinnerService,
+    private purchaseSrv: PurchaseService,
+    private sweetAlert2Srv: Sweetalert2Service,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -62,10 +70,19 @@ export class CheckoutComponent implements OnInit {
       distinctUntilChanged((prev, next) => JSON.stringify(prev) === JSON.stringify(next)),
     )
     .subscribe(cart => {
-      // console.log('cart', cart);
+      console.log('cart', cart);
+      if(!cart) { return; }
       this.cart = cart;
       this.uid = this.cart.uid;
     });
+  }
+
+  get totales() {
+    if(!this.cart) return 0;
+    if(!this.uid) return 0;
+
+    const { product = [] } = this.cart;
+    return product.map((item: any) => item.totales).reduce((a: number, b: number) => a + b, 0);
   }
 
   onSelectPaymentOption(item: any){
@@ -75,8 +92,51 @@ export class CheckoutComponent implements OnInit {
     }
   }
 
-  onPaypalCallback(event: any){
-    console.log('onPaypalCallback', event);
+  async onPaypalCallback(event: any){
+    try {
+
+      if(event.type === 'cancel'){ return; }
+
+      if(event.type === 'error'){ return; }
+
+      // console.log('onPaypalCallback', {
+      //   paypalResponse: event,
+      //   cart: this.cart,
+      //   uid: this.uid,
+      // });
+
+      await this.spinner.show();
+
+      const purchase = {
+        ...this.cart,
+        paymentMethod: 'paypal',
+        payload: event.data,
+        status: 'completed',
+        payedAt: moment().valueOf(),
+        orderId: this.cartSrv.generateId(),
+        totales: this.totales
+      };
+      console.log('purchase', purchase);
+
+      /** Almacenar orden de compra */
+      await this.purchaseSrv.storePurchase(environment.dataEvent.keyDb, purchase.orderId, purchase);
+
+      /** TODO: enviar notificación vía email de la compra realizada */
+
+      this.router.navigate(['/pages/dashboard']);
+
+      /** Eliminar carrito de compra */
+      await this.cartSrv.deleteCart(environment.dataEvent.keyDb, this.uid);
+
+      this.sweetAlert2Srv.showToast('Compra realizada satisfactoriamente', 'success');
+      return;
+      
+    } catch (err) {
+      console.log('Error on CheckoutComponent.onPaypalCallback()', err);
+      return;
+    } finally {
+      this.spinner.hide();
+    }
   }
 
   ngOnDestroy(): void {
