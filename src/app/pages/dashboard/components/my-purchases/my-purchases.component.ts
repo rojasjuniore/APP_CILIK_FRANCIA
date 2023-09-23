dd56f84804600c18dda353eeac5ce1c728bd90df
Jhonny <jhonnycgarcia@gmail.com>
2023-09-23 14:56:22 -0500

    UP

--- src/app/pages/dashboard/components/my-purchases/my-purchases.component.ts
+++ src/app/pages/dashboard/components/my-purchases/my-purchases.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-my-purchases',
   templateUrl: './my-purchases.component.html',
   styleUrls: ['./my-purchases.component.css']
 })
-export class MyPurchasesComponent implements OnInit {
+export class MyPurchasesComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public uid!: string;
+  public queries = {
+    pending: {
+      available: false,
+      query: [
+        {field: 'status', condition: '==', value: 'pending'},
+        {field: 'uid', condition: '==', value: null},
+      ]
+    },
+    completed: {
+      available: false,
+      query: [
+        {field: 'status', condition: '==', value: 'completed'},
+        {field: 'uid', condition: '==', value: null},
+      ]
+    },
+    rejected: {
+      available: false,
+      query: [
+        {field: 'status', condition: '==', value: 'rejected'},
+        {field: 'uid', condition: '==', value: null},
+      ]
+    },
+  };
+
+  private sub$!: Subscription;
+
+  constructor(
+    private authSrv: AuthenticationService,
+  ) { }
 
   ngOnInit(): void {
+
+    this.sub$ = this.authSrv.uid$.subscribe((uid) => {
+      // console.log('uid', uid);
+      if(!uid) { return; }
+      this.uid = uid;
+      this.updateValues();
+    });
+
+  }
+
+  /**
+   * Habilitar las consultas que requieren el uid del usuario
+   */
+  updateValues(){
+    const snapshot = Object.entries(this.queries).map(([key, value]) => {
+      const available = (this.uid) ? true : false;
+      const query = value.query.map((query) =>  query.field === 'uid' ? {...query, value: this.uid } : query);
+      return { [key]: {available, query} };
+    })
+    .reduce((acc, cur) => ({...acc, ...cur}), {});
+    // console.log('snapshot', snapshot);
+    this.queries = {...this.queries, ...snapshot};
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
   }
 
 }
