e031490102bc64cb07c1a64f2b22ef943c152860
Jhonny <jhonnycgarcia@gmail.com>
2023-09-27 11:52:44 -0500

    UP

--- src/app/admin/bank-transfer/components/bank-transfer-manager/bank-transfer-manager.component.ts
+++ src/app/admin/bank-transfer/components/bank-transfer-manager/bank-transfer-manager.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription, catchError, map, of, switchMap } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { PurchaseService } from 'src/app/services/purchase.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-bank-transfer-manager',
   templateUrl: './bank-transfer-manager.component.html',
   styleUrls: ['./bank-transfer-manager.component.css']
 })
-export class BankTransferManagerComponent implements OnInit {
+export class BankTransferManagerComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public orderId!: string;
+  public orderDoc: any;
+
+  private sub$!: Subscription;
+  
+  constructor(
+    private router: ActivatedRoute,
+    private authSrv: AuthenticationService,
+    private purchaseSrv: PurchaseService
+  ) {
+    const orderId = this.router.snapshot.paramMap.get('orderId');
+    // console.log('orderId', orderId);
+    this.orderId = orderId || '';
+  }
 
   ngOnInit(): void {
+
+    this.sub$ = this.authSrv.uid$
+    .pipe(
+      switchMap((uid) => this.purchaseSrv.getByEventAndId(environment.dataEvent.keyDb, this.orderId)),
+      map((order) => {
+        return (order) ? {exist: true, ...order} : {exist: false};
+      }),
+      catchError((err) => of({exist: false}))
+    )
+    .subscribe((order) => {
+      this.orderDoc = order;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
   }
 
 }
