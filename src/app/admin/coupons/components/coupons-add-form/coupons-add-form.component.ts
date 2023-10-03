bb2e6c563a871256b04c85fc6bdcc0cbcf777f2a
Jhonny <jhonnycgarcia@gmail.com>
2023-10-03 14:26:30 -0500

    UP

--- src/app/admin/coupons/components/coupons-add-form/coupons-add-form.component.ts
+++ src/app/admin/coupons/components/coupons-add-form/coupons-add-form.component.ts
@@ -119,7 +119,7 @@ export class CouponsAddFormComponent implements OnInit {
       const uid = await this.authSrv.getUIDPromise();
 
       const data = {
-        code: `${formData.code}`.trim().toLowerCase(),
+        code: `${formData.code}`.trim().toUpperCase(),
         slug: slugify(`${formData.code}`.trim().toLowerCase()),
         ownerType: formData.ownerType,
         ownerId: formData.ownerId,
