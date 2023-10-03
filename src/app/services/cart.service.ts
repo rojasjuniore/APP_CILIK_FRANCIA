161198b76f14cd2db7848b6da23774196fa41990
Jhonny <jhonnycgarcia@gmail.com>
2023-10-02 21:36:52 -0500

    UP

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -36,6 +36,7 @@ export class CartService {
       eventId: params.eventId || environment.dataEvent.keyDb,
       createdAt: params.createdAt || moment().valueOf(),
       product: params.product || [],
+      coupons: params.coupons || [],
       status: 'draw' // draw, pending, completed, rejected
     };
   }
