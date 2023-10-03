2e8602f288e0981b8271bfcea4cfcf8f855b44ef
Jhonny <jhonnycgarcia@gmail.com>
2023-10-03 16:31:44 -0500

    UP

--- src/app/shared/input-range-calendar/input-range-calendar.component.ts
+++ src/app/shared/input-range-calendar/input-range-calendar.component.ts
@@ -1,30 +1,79 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { end } from '@popperjs/core';
 import 'daterangepicker';
 
 // jquery
 import $ from 'jquery';
 import moment from 'moment';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-input-range-calendar',
   templateUrl: './input-range-calendar.component.html',
   styleUrls: ['./input-range-calendar.component.css']
 })
-export class InputRangeCalendarComponent implements OnInit, AfterViewInit {
+export class InputRangeCalendarComponent implements OnInit, AfterViewInit, OnChanges {
+
+  @Output() onUpdateDates = new Subject<string>();
 
   @Input() _id: string = 'dateRangePicker';
 
+/** https://www.daterangepicker.com/#options */
 
-  @Input() autoUpdateInput =  false;
+  @Input() autoUpdateInput =  true;
+
+  @Input() autoApply =  false;
+
+  @Input() opens =  'right'; // left, right, center
+
+  @Input() drops =  'auto'; // up, down, auto
+
+
+  @Input() buttonClasses =  'btn btn-sm';
+
+  @Input() applyButtonClasses =  'btn-primary';
+
+  @Input() cancelButtonClasses =  'btn-default';
+
+
+  @Input() cancelLabel =  'Cancel';
+  @Input() applyLabel =  'Apply';
 
 
   /** Fechas limites en formato DD/MM/YYYY */
-  @Input() startDate: string = moment().startOf('week').format('DD/MM/YYYY');
-  @Input() endDate: string = moment().endOf('week').format('DD/MM/YYYY');
+  @Input() startDate: string = moment().startOf('week').format('MM/DD/YYYY');
+  @Input() endDate: string = moment().endOf('week').format('MM/DD/YYYY');
+
+  @Input() minDate: string = moment().startOf('week').format('MM/DD/YYYY');
+  @Input() maxDate: string = moment().endOf('week').format('MM/DD/YYYY');
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    // console.log({
+    //   startDate: this.startDate,
+    //   endDate: this.endDate,
+    // });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const {
+      startDate,
+      endDate,
+      minDate,
+      maxDate,
+    } = changes;
+
+    if(startDate && startDate.currentValue) {
+      this.startDate = startDate.currentValue;
+      $(`#${this._id}`).data('daterangepicker').setStartDate(this.startDate);
+    }
+
+    if(endDate && endDate.currentValue) {
+      this.endDate = endDate.currentValue;
+      $(`#${this._id}`).data('daterangepicker').setEndDate(this.endDate);
+    }
+  }
 
   ngAfterViewInit() {
     this.buildDatepicker();
@@ -32,19 +81,39 @@ export class InputRangeCalendarComponent implements OnInit, AfterViewInit {
 
   public buildDatepicker(){
 
-    const opts = {
+    const options = {
+      autoApply: this.autoApply,
       autoUpdateInput: this.autoUpdateInput,
-      "startDate": this.startDate,
-      "endDate": this.endDate,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      minDate: this.minDate,
+      maxDate: this.maxDate,
+      opens: this.opens,
+      drops: this.drops,
+      buttonClasses: this.buttonClasses,
+      applyButtonClasses: this.applyButtonClasses,
+      cancelButtonClasses: this.cancelButtonClasses,
+      locale: { 
+        cancelLabel: this.cancelLabel,
+        applyLabel: this.applyLabel
+      } 
     };
 
-    // if(this.startDate && this.endDate) {
-    //   opts['startDate'] = this.startDate;
-    //   opts['endDate'] = this.endDate;
-    // }
+    console.log('options', options);
+    $(`#${this._id}`).daterangepicker({...options});
 
-    console.log('opts', opts);
-    $(`#${this._id}`).daterangepicker({...opts});
+    // console.log('prop', $(`#${this._id}`).data('daterangepicker'));
+
+    $(`#${this._id}`).on('apply.daterangepicker', (ev, picker) => {
+      const startDate = picker.startDate.format('YYYY-MM-DD');
+      const endDate = picker.endDate.format('YYYY-MM-DD');
+      console.log({startDate, endDate});
+      this.onUpdateDates.next(`${startDate}-${endDate}`);
+    });
+  }
+
+  show(){
+    $(`#${this._id}`).data('daterangepicker').show();
   }
 
 }
