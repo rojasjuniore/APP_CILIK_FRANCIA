92cf7d505c9e8d79c77998d76f875320c509a226
Jhonny <jhonnycgarcia@gmail.com>
2023-10-04 14:04:09 -0500

    UP

--- src/app/shared/cart-totales/cart-totales.component.ts
+++ src/app/shared/cart-totales/cart-totales.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { CartService } from 'src/app/services/cart.service';
+import { Sweetalert2Service } from 'src/app/services/sweetalert2.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-cart-totales',
@@ -9,7 +13,11 @@ export class CartTotalesComponent implements OnInit, OnChanges {
 
   @Input() cart: any;
 
-  constructor() { }
+  constructor(
+    private cartSrv: CartService,
+    private spinner: NgxSpinnerService,
+    private sweetAlert2Srv: Sweetalert2Service,
+  ) { }
 
   ngOnInit(): void {
   }
@@ -23,6 +31,11 @@ export class CartTotalesComponent implements OnInit, OnChanges {
     }
   }
 
+  get coupons() {
+    if(!this.cart) return [];
+    return this.cart.coupons || [];
+  }
+
   get subTotal(){
     if(!this.cart) return 0;
     const total = this.cart.product.map((item: any) => item.totales)
@@ -33,7 +46,7 @@ export class CartTotalesComponent implements OnInit, OnChanges {
   get discount() {
     if(!this.cart) return 0;
 
-    const coupons = this.cart.coupons || [];
+    const coupons = this.coupons;
     const total = coupons.map((item: any) => (item.type === 'percent') ? this.subTotal * (item.value / 100) : item.value)
     .reduce((prev: any, next: any) => Number(prev) + Number(next), 0);
     return total;
@@ -41,6 +54,23 @@ export class CartTotalesComponent implements OnInit, OnChanges {
 
   get totales(){
     return this.subTotal - this.discount;
+  }
+
+  async removeCoupon(coupon: any) {
+    try {
+      await this.spinner.show();
+
+      await this.cartSrv.removeOnCart(environment.dataEvent.keyDb, this.cart.uid, coupon, 'coupons');
+
+      this.sweetAlert2Srv.showToast('Coupon removed', 'success');
+      return;
+      
+    } catch (err) {
+      console.log('Error on CartTotalesComponent.removeCoupon', err);
+      return;
+    } finally {
+      this.spinner.hide();
+    }
   }
 
 }
