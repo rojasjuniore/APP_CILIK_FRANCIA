866ecf01a62bd55092a2de45e485a1f87f58e586
Jhonny <jhonnycgarcia@gmail.com>
2023-10-04 10:03:57 -0500

    UP

--- src/app/shared/header-welcome/header-welcome.component.ts
+++ src/app/shared/header-welcome/header-welcome.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Observable, Subscription, tap } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
@@ -7,20 +8,43 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
   templateUrl: './header-welcome.component.html',
   styleUrls: ['./header-welcome.component.css']
 })
-export class HeaderWelcomeComponent implements OnInit {
+export class HeaderWelcomeComponent implements OnInit, OnDestroy {
 
   @Input() backTo = '/pages/dashboard';
 
   public profile$!: Observable<any>;
+  public showDashboardBtn = false;
+
+  private sub$!: Subscription;
 
   constructor(
     private authSrv: AuthenticationService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
     this.profile$ = this.authSrv.userDoc$.pipe(
       // tap((data) => console.log('data', data)),
     );
+
+    this.sub$ = this.router.events.subscribe((event: any) => {
+      const { type } = event;
+
+      /** Si termina de cargar la ruta */
+      if(type === 1) {
+        const { url } = event;
+
+        this.showDashboardBtn = !['/pages/dashboard', '/admin/dashboard'].includes(url);
+        // console.log('url', url);
+        // console.log('this.showDashboardBtn', this.showDashboardBtn);
+        // console.log('event', event);
+      }
+
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
   }
 
 }
