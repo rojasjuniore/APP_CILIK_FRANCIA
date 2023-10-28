74fe17e8eecd8d07b2a83bd0728635c0e905e269
0x1junior <116573955+0x1junior@users.noreply.github.com>
2023-10-27 19:15:28 -0500

    button de admin

--- src/app/shared/header-welcome/header-welcome.component.ts
+++ src/app/shared/header-welcome/header-welcome.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { map, Observable, Subscription, tap } from 'rxjs';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { PermissionService } from 'src/app/services/permission.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-header-welcome',
@@ -16,13 +18,16 @@ export class HeaderWelcomeComponent implements OnInit, OnDestroy {
   public showDashboardBtn = false;
 
   private sub$!: Subscription;
-
+  _isAdmin = false;
   constructor(
+    private permissionSrv: PermissionService,
     private authSrv: AuthenticationService,
     private router: Router,
   ) { }
 
   ngOnInit(): void {
+
+
     this.profile$ = this.authSrv.userDoc$.pipe(
       // tap((data) => console.log('data', data)),
     );
@@ -31,7 +36,7 @@ export class HeaderWelcomeComponent implements OnInit, OnDestroy {
       const { type } = event;
 
       /** Si termina de cargar la ruta */
-      if(type === 1) {
+      if (type === 1) {
         const { url } = event;
 
         this.showDashboardBtn = !['/pages/dashboard', '/admin/dashboard'].includes(url);
@@ -39,9 +44,23 @@ export class HeaderWelcomeComponent implements OnInit, OnDestroy {
         // console.log('this.showDashboardBtn', this.showDashboardBtn);
         // console.log('event', event);
       }
-
     });
+
+    this.isAdmin()
   }
+
+  isAdmin() {
+    const uid: any = localStorage.getItem('uid');
+    this.permissionSrv.getUserEventFullRolesObservable(environment.dataEvent.keyDb, uid)
+      .subscribe((data: any) => {
+        console.log('data', data);
+        this._isAdmin = data.roles.length > 0 ? true : false;
+        console.log('this._isAdmin', this._isAdmin);
+      });
+  }
+
+
+
 
   ngOnDestroy(): void {
     this.sub$.unsubscribe();
