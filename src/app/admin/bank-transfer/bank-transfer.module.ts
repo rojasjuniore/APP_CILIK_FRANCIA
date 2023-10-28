a08ac4b1b402fbaab13c38202251f78eedd18fa8
0x1junior <116573955+0x1junior@users.noreply.github.com>
2023-10-28 16:03:47 -0500

    bakn

--- src/app/admin/bank-transfer/bank-transfer.module.ts
+++ src/app/admin/bank-transfer/bank-transfer.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 import { BankTransferRoutingModule } from './bank-transfer-routing.module';
 import { BankTransferComponent } from './bank-transfer.component';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { BankTransferManagerComponent } from './components/bank-transfer-manager/bank-transfer-manager.component';
 
 
