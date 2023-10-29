1daf2a8bb8ce692da012aa1a1ec0a4d9fc8608ed
0x1junior <116573955+0x1junior@users.noreply.github.com>
2023-10-29 15:43:31 -0500

    cupdtas

--- src/app/services/dedicates/installment.service.ts
+++ src/app/services/dedicates/installment.service.ts
@@ -16,133 +16,61 @@ export class InstallmentService {
 
   public installmentDateLimit = '2023-02-02';
 
-  // public installments = [
-  //   {
-  //     installmentOrder: 0,
-  //     ranges: { from: '2023-08-01', to: '2023-11-09' },
-  //     limit: { year: 2023, month: 12, day: 2 },
-  //     nroInstallments: 4,
-  //   },
-  //   {
-  //     installmentOrder: 1,
-  //     ranges: { from: '2023-11-10', to: '2023-12-15' },
-  //     limit: { year: 2023, month: 12, day: 2 },
-  //     nroInstallments: 3,
-  //   },
-  //   {
-  //     installmentOrder: 2,
-  //     ranges: { from: '2023-12-16', to: '2024-01-19' },
-  //     limit: { year: 2023, month: 12, day: 2 },
-  //     nroInstallments: 2,
-  //   },
-  // ]
+
 
   constructor() { }
 
 
   /**
-   * @dev  Método para calcular las cuotas
-   * @param todayDate 
-   * @returns 
-   */
+  * Función para calcular las fechas de las cuotas de pago basadas en la fecha actual.
+  * @param todayDate - La fecha actual en formato de cadena 'YYYY-MM-DD'.
+  * @returns Un array de objetos que representan las cuotas a pagar.
+  */
   calculateQuotas(todayDate: string): Quota[] {
+    // Convertir la fecha actual a un objeto moment para manipulación fácil de fechas.
     const today = moment(todayDate);
+
+    // Inicializar el array que contendrá las cuotas.
     const quotas: Quota[] = [];
 
+    // Primer caso: Si la fecha actual es antes del 10 de noviembre de 2023.
     if (today.isBefore(moment('2023-11-10'))) {
-      const firstQuotaDate = today.clone().add(1, 'month').date(2);
-      quotas.push({ date: firstQuotaDate.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 1 });
-      quotas.push({ date: firstQuotaDate.clone().add(1, 'month').format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 2 });
-      quotas.push({ date: firstQuotaDate.clone().add(2, 'months').format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 3 });
-    } else if (today.isBefore(moment('2023-12-16'))) {
-      const firstQuotaDate = today.clone().add(1, 'month').date(2);
-      quotas.push({ date: firstQuotaDate.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 1 });
-      quotas.push({ date: firstQuotaDate.clone().add(1, 'month').format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 2 });
-    } else if (today.isBefore(moment('2023-01-20'))) {
-      quotas.push({ date: today.clone().add(1, 'month').date(2).format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 1 });
+      // Calcular la fecha para la primera cuota, asegurándose de que haya al menos 30 días de diferencia y no pase del 2 del mes siguiente.
+      const firstQuotaDate = moment.max(today.clone().add(30, 'days'), today.clone().add(1, 'month').date(2));
+      // Calcular las fechas para las cuotas siguientes con la misma lógica.
+      const secondQuotaDate = moment.max(firstQuotaDate.clone().add(30, 'days'), firstQuotaDate.clone().add(1, 'month').date(2));
+      const thirdQuotaDate = moment.max(secondQuotaDate.clone().add(30, 'days'), secondQuotaDate.clone().add(1, 'month').date(2));
+      const finalQuotaDate = moment.min(thirdQuotaDate.clone().add(30, 'days'), moment('2024-02-02'));
+
+      // Agregar las cuotas al array de cuotas.
+      quotas.push({ date: today.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 1 });
+      quotas.push({ date: firstQuotaDate.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 2 });
+      quotas.push({ date: secondQuotaDate.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 3 });
+      quotas.push({ date: finalQuotaDate.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 4 });
+    }
+    // Segundo caso: Si la fecha actual es antes del 16 de diciembre de 2023.
+    else if (today.isBefore(moment('2023-12-16'))) {
+      // Calcular las fechas para las cuotas con la misma lógica que en el primer caso.
+      const firstQuotaDate = moment.max(today.clone().add(30, 'days'), today.clone().add(1, 'month').date(2));
+      const secondQuotaDate = moment.max(firstQuotaDate.clone().add(30, 'days'), firstQuotaDate.clone().add(1, 'month').date(2));
+      const finalQuotaDate = moment.min(secondQuotaDate.clone().add(30, 'days'), moment('2024-02-02'));
+
+      // Agregar las cuotas al array de cuotas.
+      quotas.push({ date: today.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 1 });
+      quotas.push({ date: firstQuotaDate.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 2 });
+      quotas.push({ date: finalQuotaDate.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 3 });
+    }
+    // Tercer caso: Si la fecha actual es antes del 20 de enero de 2024.
+    else if (today.isBefore(moment('2024-01-20'))) {
+      // Agregar las cuotas al array de cuotas, la segunda cuota es fija al 2 de febrero de 2024.
+      quotas.push({ date: today.format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 1 });
+      quotas.push({ date: moment('2024-02-02').format('YYYY-MM-DD'), status: 'pending', amount: 0, paymentMethod: null, quota: 2 });
     }
 
+    // Retornar el array de cuotas.
     return quotas;
   }
 
-
-
-  // /**
-  //  * 
-  //  * @param date 
-  //  * @param ranges 
-  //  * @returns 
-  //  */
-  // isDateInRange(date: string, ranges: any) {
-  //   const dateMoment = moment(date);
-  //   const fromMoment = moment(ranges.from);
-  //   const toMoment = moment(ranges.to);
-
-  //   return dateMoment.isBetween(fromMoment, toMoment, null, '[]');
-  // }
-
-
-  // /**
-  //  * 
-  //  * @param date 
-  //  * @returns 
-  //  */
-  // getInstallmentByDate(date: string) {
-  //   const installments: any[] = [{
-  //     quota: 1,
-  //     date: date,
-  //     paymentMethod: null,
-  //     status: 'pending',
-  //     amount: 0,
-  //   }];
-
-  //   const snapshot = this.installments.find((item) => {
-  //     return this.isDateInRange(date, item.ranges);
-  //   });
-
-  //   if (!snapshot) return installments;
-
-  //   const dateLimit = moment()
-  //     .set('date', snapshot.limit.day)
-  //     .set('month', snapshot.limit.month - 1)
-  //     .set('year', snapshot.limit.year)
-  //     .format('YYYY-MM-DD');
-  //   // console.log('dateLimit', dateLimit);
-
-  //   for (let index = 1; index < snapshot.nroInstallments; index++) {
-  //     let snapshotDate = moment(date).add(index, 'month').format('YYYY-MM-DD');
-
-  //     if (moment(snapshotDate).isAfter(dateLimit)) {
-  //       snapshotDate = moment(snapshotDate).set('date', snapshot.limit.day).format('YYYY-MM-DD');
-  //     }
-
-  //     installments.push({
-  //       quota: index + 1,
-  //       date: snapshotDate,
-  //       paymentMethod: null,
-  //       status: 'pending',
-  //       amount: 0,
-  //     });
-  //   }
-
-  //   console.log('snapshot', snapshot);
-  //   console.log('installments', installments);
-
-  //   return installments;
-  // }
-
-
-
-
-
-
-  // addQuota(date) {
-  //   quotas.push({
-  //     "date": date.format('YYYY-MM-DD'),
-  //     "status": "pending",
-  //     "amount": 34
-  //   });
-  // }
 
 
 }
