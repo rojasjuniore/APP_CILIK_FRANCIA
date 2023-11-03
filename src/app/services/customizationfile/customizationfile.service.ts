2d77aeea390ab9406a45a1ad605258ca39cad20e
0x1junior <116573955+0x1junior@users.noreply.github.com>
2023-11-03 16:06:05 -0500

    melo

--- src/app/services/customizationfile/customizationfile.service.ts
+++ src/app/services/customizationfile/customizationfile.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { environment } from "src/environments/environment";
-
 @Injectable({
   providedIn: "root",
 })
