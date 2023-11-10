c4fe8f3823e61908b5d4027904f16e921d89fe74
0x1junior <116573955+0x1junior@users.noreply.github.com>
2023-11-09 19:28:00 -0500

    Update version and add staging deployment

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Sweetalert2Service } from './sweetalert2.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export class VersionService {
 
   checkForUpdates(localVersion: string) {
     // La URL de tu función desplegada de Firebase Functions
-    this.db.object('version-wldc').valueChanges().subscribe(async (data: any) => {
+    this.db.object('version-wldc' + environment.dataEvent.keyDb).valueChanges().subscribe(async (data: any) => {
       console.log('version', data['version']);
       if (data['version'] !== localVersion) {
 
