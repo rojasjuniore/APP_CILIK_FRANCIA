dde93751811ed0b86ab53d7daa89790f83335ee0
0x1junior <116573955+0x1junior@users.noreply.github.com>
2023-12-04 12:37:50 -0500

    Update environment.ts and purchase-list-item-card-admin.component.html

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   version: '0.0.46',
   dataEvent: {
-    keyDb: "iS30s4eNrRfG3dbeUV18",
+    keyDb: "QwksBFqsPZT9itRwkpUB",
     appURL: "https://dev-wldc.web.app",
     name: "WORLD LATIN DANCE CUP Cartagena 2024",
     termsAndConditions: "",
