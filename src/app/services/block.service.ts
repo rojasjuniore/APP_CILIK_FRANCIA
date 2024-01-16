0a252fa3e4eb22a145632db5eb1bddea3af3cbb6
Jhonny <jhonnycgarcia@gmail.com>
2024-01-16 17:20:27 -0500

    UP Add getBlockByType method to BlockService

--- src/app/services/block.service.ts
+++ src/app/services/block.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -20,4 +21,30 @@ export class BlockService {
         })
     })
   }
+
+  /**
+   * @dev Obtener listado de bloques filtrado por tipo de categoria
+   * @param keyDb           Key de la base de datos
+   * @param subCategory     Subcategoria
+   * @returns 
+   */
+  async getBlockByType(keyDb: string, subCategory: string) {
+    try {
+      const params = new HttpParams({
+        fromObject: {
+          keyDb: keyDb,
+          subCategory: subCategory
+        }
+      });
+
+      const url = `${environment.urlrootFunctions}/v2/division/block-by-type` + '?' + params.toString();
+      const snapshot: any = await lastValueFrom(this.http.get( url ) );
+      return snapshot.results;
+      
+    } catch (err) {
+      console.log('Error on DivisionPurchasePage.ngOnInit', err);
+      throw err;
+    }
+  }
+
 }
